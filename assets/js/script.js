93a75175cf47b246b26c94168420164f635c1e02
Kasem Al <xdow69x0@students.codeinstitute.net>
2024-11-29 08:55:32 +0000

    readd Dom eventListener and declare const elements

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function (){
+    //console.log('ok')
+    const buttons = document.getElementsByTagName("button")
+    const message = document.getElementById("message")
+    const playerScore = document.getElementById("player-score")
+    const computerScore = document.getElementById("computer-score")
+    const playerImage = document.getElementById("player-image")
+    const computererImage = document.getElementById("computer-image")
+    const choices=['rock','papaer','scissors']
+})
