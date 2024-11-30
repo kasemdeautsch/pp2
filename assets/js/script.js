75b6b291dbbc32164348f210bfc3723002232c4d
Kasem Al <xdow69x0@students.codeinstitute.net>
2024-11-30 16:53:52 +0000

    some tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /**
- * constants for dom elements
+ * constants for dom elements and possible choices added in an array
  */
 const buttons = document.getElementsByTagName("button");
 const message = document.getElementById("message");
@@ -7,14 +7,14 @@ const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computererImage = document.getElementById("computer-image");
-/**
- * Possible choices added in an array
- */
 const choices=['rock','paper','scissors'];
+console.log(playerScore);
+
 /**
  * wait untill dom loaded succesfully then add listeners on buttons
  */
 document.addEventListener("DOMContentLoaded", function (){
+    console.log(playerScore);
     /**
      * loop over the buttons and add the listener on each
      */
@@ -22,41 +22,38 @@ document.addEventListener("DOMContentLoaded", function (){
         button.addEventListener('click',function (){
             let playerChoice = this.getAttribute('choice');
             runGame(playerChoice);
-
     })
 }
 
+
 });
+console.log(playerScore);
 /** 
  * main function receives one parameter which is the value 
  * of the custom attribute (choice) applied to the buttons to recognize the type of selection.
  * Inspired from Love maths Project
 */
 function runGame(playerChoice){
-    //let x=`123`
-    //console.log(x, typeof x);
+    console.log(playerScore);
     playerImage.src = `assets/images/${choices[playerChoice]}.png`;
-    //console.log(playerImage.src, typeof playerImage.src)
     playerImage.alt = choices[playerChoice];
-     //console.log(playerImage.alt, typeof playerImage.alt);
-     //playerImage.style.height = "40%";
-     //playerImage.style.height = "70px";
+
     let randIndex=Math.floor(Math.random()*choices.length);
-     //console.log(rand);
     computererImage.src = `assets/images/${choices[randIndex]}.png`;
-     //console.log(computererImage.src, typeof computererImage.src)
     computererImage.alt = choices[randIndex];
-     //console.log(computererImage.alt, typeof computererImage.alt);
+
     let playerOption=choices[playerChoice];
     let computerOption=choices[randIndex];
     let result= checkWinner(playerOption,computerOption);
     updateScore(result);
 }
 /**
- * function tekes tow parameters and returns the result as string 
+ * function tekes tow parameters whitch are values of the 'choices' array
+ * and returns the result as string 
  * to determine the winner(player/computer)
  */
 function checkWinner(playerOption,computerOption){
+    console.log(playerScore);
         //console.log(playerImage.alt, typeof playerImage.alt);
         let winner="";
         if(playerOption=== "scissors"){
@@ -93,12 +90,19 @@ function checkWinner(playerOption,computerOption){
         }
         
     }
-    
+/**
+ * function receives the winner to display the message in message div
+ * and update the score of both player and computer.
+ */
 function updateScore(winner){
+    //console.log(playerScore);
+    //playerScore='2';
+    //console.log(playerScore.textContent,typeof playerScore.textContent);
     let currenPlayertScore = parseInt(playerScore.textContent);
-    console.log(currenPlayertScore);
+    //console.log(currenPlayertScore,typeof currenPlayertScore);
+    //console.log(currenPlayertScore);
     let currenComputertScore = parseInt(computerScore.textContent);
-    console.log(currenComputertScore);
+    //console.log(currenComputertScore);
     if (winner === "player"){
         message.textContent = "You win!";
         playerScore.textContent = ++currenPlayertScore;
