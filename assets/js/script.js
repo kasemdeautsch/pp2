99e0db59a8f78875c40c9c01067ddd94315237e1
Kasem Al <xdow69x0@students.codeinstitute.net>
2024-11-30 10:45:10 +0000

    add comments to some functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,61 @@
+/**
+ * constants for dom elements
+ */
 const buttons = document.getElementsByTagName("button");
-    const message = document.getElementById("message");
-    const playerScore = document.getElementById("player-score");
-    //console.log(typeof playerScore.textContent);
-    const computerScore = document.getElementById("computer-score");
-    //console.log(typeof computerScore.textContent);
-    const playerImage = document.getElementById("player-image");
-    const computererImage = document.getElementById("computer-image");
-    const choices=['rock','paper','scissors'];
+const message = document.getElementById("message");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computererImage = document.getElementById("computer-image");
+/**
+ * Possible choices added in an array
+ */
+const choices=['rock','paper','scissors'];
+/**
+ * wait untill dom loaded succesfully then add listeners on buttons
+ */
 document.addEventListener("DOMContentLoaded", function (){
-    //console.log('ok')
-    
+    /**
+     * loop over the buttons and add the listener on each
+     */
     for (let button of buttons){
         button.addEventListener('click',function (){
-            let playerChoice = this.getAttribute('choice')
-            //playerImage
-            //console.log(playerImage);
-            //console.log(playerChoice);
-            runGame(playerChoice)
+            let playerChoice = this.getAttribute('choice');
+            runGame(playerChoice);
 
     })
 }
 
 });
-//console.log(playerScore.textContent);
+/** 
+ * main function receives one parameter which is the value 
+ * of the custom attribute (choice) applied to the buttons to recognize the type of selection.
+ * Inspired from Love maths Project
+*/
 function runGame(playerChoice){
     //let x=`123`
     //console.log(x, typeof x);
     playerImage.src = `assets/images/${choices[playerChoice]}.png`;
     //console.log(playerImage.src, typeof playerImage.src)
-     playerImage.alt = choices[playerChoice];
+    playerImage.alt = choices[playerChoice];
      //console.log(playerImage.alt, typeof playerImage.alt);
      //playerImage.style.height = "40%";
      //playerImage.style.height = "70px";
-     let randIndex=Math.floor(Math.random()*choices.length);
+    let randIndex=Math.floor(Math.random()*choices.length);
      //console.log(rand);
-     computererImage.src = `assets/images/${choices[randIndex]}.png`;
+    computererImage.src = `assets/images/${choices[randIndex]}.png`;
      //console.log(computererImage.src, typeof computererImage.src)
-     computererImage.alt = choices[randIndex];
+    computererImage.alt = choices[randIndex];
      //console.log(computererImage.alt, typeof computererImage.alt);
     let playerOption=choices[playerChoice];
     let computerOption=choices[randIndex];
     let result= checkWinner(playerOption,computerOption);
     updateScore(result);
 }
-
+/**
+ * function tekes tow parameters and returns the result as string 
+ * to determine the winner(player/computer)
+ */
 function checkWinner(playerOption,computerOption){
         //console.log(playerImage.alt, typeof playerImage.alt);
         let winner="";
