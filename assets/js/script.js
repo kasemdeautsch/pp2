5ab6beaef150149e9cf111e8dea0c9b4d6128fbd
Kasem Al <xdow69x0@students.codeinstitute.net>
2024-11-30 10:26:41 +0000

    create updateScore function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const buttons = document.getElementsByTagName("button");
     const message = document.getElementById("message");
     const playerScore = document.getElementById("player-score");
+    //console.log(typeof playerScore.textContent);
     const computerScore = document.getElementById("computer-score");
+    //console.log(typeof computerScore.textContent);
     const playerImage = document.getElementById("player-image");
     const computererImage = document.getElementById("computer-image");
     const choices=['rock','paper','scissors'];
@@ -20,40 +22,79 @@ document.addEventListener("DOMContentLoaded", function (){
 }
 
 });
-
+//console.log(playerScore.textContent);
 function runGame(playerChoice){
     //let x=`123`
     //console.log(x, typeof x);
     playerImage.src = `assets/images/${choices[playerChoice]}.png`;
     //console.log(playerImage.src, typeof playerImage.src)
-     playerImage.alt = `${choices[playerChoice]}`;
+     playerImage.alt = choices[playerChoice];
      //console.log(playerImage.alt, typeof playerImage.alt);
      //playerImage.style.height = "40%";
      //playerImage.style.height = "70px";
-     let rand=Math.floor(Math.random()*choices.length);
+     let randIndex=Math.floor(Math.random()*choices.length);
      //console.log(rand);
-     computererImage.src = `assets/images/${choices[rand]}.png`;
+     computererImage.src = `assets/images/${choices[randIndex]}.png`;
      //console.log(computererImage.src, typeof computererImage.src)
-     computererImage.alt = choices[rand];
-     console.log(computererImage.alt, typeof computererImage.alt);
+     computererImage.alt = choices[randIndex];
+     //console.log(computererImage.alt, typeof computererImage.alt);
+    let playerOption=choices[playerChoice];
+    let computerOption=choices[randIndex];
+    let result= checkWinner(playerOption,computerOption);
+    updateScore(result);
+}
 
-    if(playerImage.alt === "scissors"){
-        if(computererImage.alt === "paper"){
-            message.textContent = "You win!";
+function checkWinner(playerOption,computerOption){
+        //console.log(playerImage.alt, typeof playerImage.alt);
+        let winner="";
+        if(playerOption=== "scissors"){
+            if(computerOption === "paper"){
+                winner= "player";
+                return winner;
+                //message.textContent = "You win!";
+            }else{
+                winner= "computer";
+                return winner;
+                //message.textContent = "Computer wins!";
+            }
+        }else if(playerOption === "paper"){
+            if(computerOption === "rock"){
+                winner= "player";
+                return winner;
+            }else{
+                winner= "computer";
+                //message= "You win!";
+                return winner;
+            }
+        }else if(playerOption === "rock"){
+            if(computerOption === "scissors"){
+                winner= "player";
+                return winner;
+            }else{
+                winner= "computer";
+                //message= "You win!";
+                return winner;
+            }
         }else{
-            message.textContent = "Computer wins!";
+            alert (`Anknoun option!`);
+            throw `Anknoun option!. Aborting`;
         }
-    }else if(playerImage.alt === "paper"){
-        if(computererImage.alt === "rock"){
-            message.textContent = "You win!"
-        }else{
-            message.textContent = "Computer wins!"
-        }
-    }else if(playerImage.alt === "rock"){
-        if(computererImage.alt === "scissors"){
-            message.textContent = "You win!"
-        }else{
-            message.textContent = "Computer wins!"
-        }
+        
+    }
+    
+function updateScore(winner){
+    let currenPlayertScore = parseInt(playerScore.textContent);
+    console.log(currenPlayertScore);
+    let currenComputertScore = parseInt(computerScore.textContent);
+    console.log(currenComputertScore);
+    if (winner === "player"){
+        message.textContent = "You win!";
+        playerScore.textContent = ++currenPlayertScore;
+    }else if(winner === "computer"){
+        message.textContent = "Computer wins!";
+        computerScore.textContent = ++currenComputertScore;
+    }else{
+        alert (`Anknoun ${winner}!`);
+        throw `Anknoun ${winner}! Aborting`;
     }
 }
